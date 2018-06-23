fb5f71c7e6ba3d029bf00b955a33b8a687dbf71f
Jose Torres <xaerofox@gmail.com>
2018-06-23 17:22:00 -0400

    Created player GameObject with basic movement

---
+++ BlockEvadeGame/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 7;
+    private float screenHalfWidthInWorldUnits;
+
+    // Use this for initialization
+    void Start()
+    {
+        float halfPlayerWidth = transform.localScale.x / 2;
+        screenHalfWidthInWorldUnits = Camera.main.aspect * Camera.main.orthographicSize + halfPlayerWidth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputX = Input.GetAxisRaw("Horizontal");
+        float velocity = inputX * speed;
+        transform.Translate(Vector2.right * velocity * Time.deltaTime);
+
+        if (transform.position.x < -screenHalfWidthInWorldUnits)
+            transform.position = new Vector2(screenHalfWidthInWorldUnits, transform.position.y);
+        if (transform.position.x > screenHalfWidthInWorldUnits)
+            transform.position = new Vector2(-screenHalfWidthInWorldUnits, transform.position.y);
+    }
+}
