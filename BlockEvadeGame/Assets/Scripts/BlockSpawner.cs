acdd003e5adc4d7176f0111a06bbc38c205f9b12
Jose Torres <xaerofox@gmail.com>
2018-06-23 18:00:15 -0400

    Added falling blocks with size/angle variants

---
+++ BlockEvadeGame/Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+
+    public GameObject fallBlockPrefab;
+    public float spawnDelayInSeconds = 1;
+    public float spawnAngleMax;
+         
+
+    float nextSpawnTime;
+    Vector2 screenHalfSizeWorldUnits;
+    public Vector2 spawnSizeMinMax;
+
+	// Use this for initialization
+	void Start () {
+        screenHalfSizeWorldUnits = new Vector2(Camera.main.aspect * Camera.main.orthographicSize, Camera.main.orthographicSize);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Time.time > nextSpawnTime)
+        {
+            nextSpawnTime = Time.time + spawnDelayInSeconds;
+
+            float spawnSize = Random.Range(spawnSizeMinMax.x, spawnSizeMinMax.y);
+            float spawnAngle = Random.Range(-spawnAngleMax, spawnAngleMax);
+
+            Vector2 spawnPosition = new Vector2(Random.Range(-screenHalfSizeWorldUnits.x, screenHalfSizeWorldUnits.x), screenHalfSizeWorldUnits.y + spawnSize);
+            GameObject newBlock = (GameObject)Instantiate(fallBlockPrefab, spawnPosition, Quaternion.Euler(Vector3.forward * spawnAngle));
+            newBlock.transform.localScale = Vector2.one * spawnSize;
+        }
+	}
+}
